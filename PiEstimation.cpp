a66a5cd502be00292f4657387687ed41e8f9310f
Prateek Narendra <prateeksapple@gmail.com>
2014-09-30 01:36:53 +0530

    Bug fixes in Pi Estimation and calculation of Integration of linear equations

--- PiEstimation.cpp
+++ PiEstimation.cpp
@@ -3,26 +3,33 @@
 #include <stdlib.h>
 #include "/home/pnpninja/PC/Monte Carlo/prng_engine.hpp"
 #include "mpi.h"
-#define sitmo_rand_max 5000000000
+#define sitmo_rand_max 4294967295
 using namespace std;
+
 int main(int argc,char* argv[])
 {
-	int numprocs,myid,rc;
+  //Initialization of variables
+  int numprocs,myid,rc;
   long int all_valid_points;
-  double iterations,eachpi,pisum,pi;
-	//Initialization code
+  double iterations,eachpi,pisum,pi,start,total_time;
+	//Initialization on MPI Variables
   MPI_Init(&argc,&argv);
   MPI_Status status;
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+  //Get number of iterations in MASTER Process / Process 0
   if(myid == 0)
   {
       cout<<"Enter the number of iterations in each thread: ";
       cin>>iterations;
+      start = MPI_Wtime();
   }
+  //Broadcast it to all other processes
   MPI_Bcast(&iterations,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
-  sitmo::prng_engine eng(myid);
+  //Parallel Random Generator with seed as MPI_Wtime()
+  sitmo::prng_engine eng(MPI_Wtime());
   long int valid_points = 0;
+  //Calculate how many points are inside
   for(int a=1;a<=iterations;a++)
   {
     double x = (double(eng())/(double(sitmo_rand_max)));
@@ -32,15 +39,18 @@ int main(int argc,char* argv[])
     if((x*x)+(y*y)<=1.0)
       valid_points++;  
   }
-  eachpi = 4.0 * (double)valid_points / (double)iterations - 0.38;
-  printf("\n%lf",eachpi);
+  //Calculate each Pi generated in each process and print it
+  eachpi = 4.0 * (double)valid_points / (double)iterations;
+  printf("\nThread %d : %lf",myid,eachpi);
   rc = MPI_Reduce(&eachpi,&pisum,1, MPI_DOUBLE, MPI_SUM,0, MPI_COMM_WORLD);
   if(myid == 0)
   {
-    
+    //Average of Pi
     double pi = pisum/(double(numprocs));
+    total_time = MPI_Wtime() - start;
     cout<<"\nValue of Pi by Dartboard Method is "<<pi;
-    cout<<"\nError is "<<pi-3.14159265358979323846;
+    cout<<"\nError is : "<<pi-3.14159265358979323846;
+    cout<<"\nTime of execution : "<<total_time<<"\n";
   }
   MPI_Finalize();
   
